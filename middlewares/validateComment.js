0e6a7a1e78e5e3fd3cf71e385f844b45b9dd7bbc
MeriamEsprit <meriem.hjiri@esprit.tn>
2023-07-06 01:45:42 +0100

    init tunivita back-end

---
+++ middlewares/validateComment.js
@@ -0,0 +1,18 @@
+const yup = require("yup");
+const validate = async(req, res, next) => {
+   try {
+    console.log(req.body);
+    const schema = yup.object().shape({
+
+        text: yup.string().required(),
+        post: yup.string().required(),
+    });
+    await schema.validate(req.body);
+    next();
+   } catch (error) {
+    res.json(error.message);
+    console.log(error.message);
+
+   }
+}
+module.exports = validate;
---
+++ routes/comment.js
@@ -0,0 +1,77 @@
+var express = require('express');
+const commentModel = require("../models/comment");
+const userModel = require("../models/user");
+const postModel = require("../models/post");
+var router = express.Router();
+
+
+router.post("/add",async (req, res, next) => {
+    try {
+      const { Postid } = req.params;
+      const userId = req.body.currentUser;
+      var user = await userModel.findById(userId);
+
+      const {text} = req.body;
+      var post = await postModel.findById(Postid);
+      if (!post_comment) {
+        throw new Error("post does not exist!");
+      }
+        
+      const comment = new commentModel({
+        text: text,
+        post: post,
+        user:user,
+      });
+
+      comment.save();
+      res.json(comment);
+    } catch (error) {
+      res.json(error.message);
+    }
+  }
+);
+
+router.get("/delete/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await commentModel.findByIdAndDelete(id);
+    res.json("comment deleted");
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+
+
+
+router.get("/get/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const comment = await commentModel.findById(id);
+    res.json(comment);
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+
+router.post("/update/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { text } = req.body;
+    var commentaire = await commentModel.findById(id);
+    console.log(commentaire);
+    const com=await commentModel.findByIdAndUpdate(id,req.body);
+    res.json(com);
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+router.get("/get", async (req, res, next) => {
+  try {
+    const comments = await commentModel.find();
+    res.json(comments);
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+
+module.exports = router;
